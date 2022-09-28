ebc9a786da67b4c1e8cecf6e50e2e1be9a780984
alexcaussades <alexcaussades@gmail.com>
2022-09-29 00:08:58 +0200

    enregistrement friends validate

--- module/ia/friend.py
+++ module/ia/friend.py
@@ -2,13 +2,16 @@ import sqlite3
 import os
 import logging
 import socket
+import json
 from pathlib import Path
 
 
-docs = "datacliivao"
+docs = "info-ivao"
+dossier_data = "friends"
+path = Path.home()
 
 def pathData():
-    return os.path.join(Path.home())
+    return os.path.join(path, "AppData", "Roaming", docs, dossier_data)
 
 def fiends_pilote(vid, p):
     for a in range(0, len(p)):
@@ -21,12 +24,45 @@ def fiends_atc(vid, x):
             print("Callsign: " + x[a]["callsign"])
 
 def get_dossier():
-    if(os.path.exists(pathData())):
-        with open(pathData(), "r") as data:
-            return data
+    if os.path.exists(pathData()):
+        return True
     else:
-        return os.mkdir(pathData())
+        os.makedirs(pathData(), mode=0o777)
+        
 
+# get_dossier()
+# dic = {"vid": 191514, "name": "Caussades", "lasname": "Alexandre"}
+# creatfile = json.dumps(dic, indent=4)
+# localfile = open(pathData()+"/"+ str(dic["vid"])+".json", "w")
+# localfile.write(creatfile)
+# localfile.close()
 
-#print(get_dossier())
-print(pathData())
+class add_friends():
+
+    def __init__(self, vid, name=None, lastname=None):
+        self.vid = vid
+        self.name = name
+        self.lastname = lastname
+    
+    def srvid(self):
+        return self.vid
+    
+    def creatdic(self):
+        dic = {"vid": self.vid, "name": self.name, "lastname": self.lastname}
+        return dic
+
+    def creatjson(self):
+        creatfile = json.dumps(self.creatdic(), indent=4)
+        localfile = open(pathData()+"/"+ str(self.srvid())+".json", "w")
+        localfile.write(creatfile)
+        localfile.close()
+        try:
+            with open (pathData()+"/"+ str(self.srvid())+".json", "r"): pass
+            print("Add your friends successful")
+
+        except IOError:
+            print("Add friends is bad ")
+
+# p1 = add_friends(191520, "thyrerryy", "qskdkqizdjq")
+# print(p1.creatjson())
+
