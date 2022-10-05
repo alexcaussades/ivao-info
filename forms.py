70d2897fde52d660d090de5fab7eadd4e9e6f090
alexcaussades <alexcaussades@gmail.com>
2022-10-05 23:21:48 +0200

    sauv interface

--- forms.py
+++ forms.py
@@ -1,40 +1,51 @@
-from PySide6.QtWidgets import QApplication, QWidget, QFrame, QHBoxLayout, \
-    QVBoxLayout, QPushButton, QGridLayout, QLabel
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QFrame, QHBoxLayout, QVBoxLayout, QPushButton, QGridLayout, QLabel, QLineEdit, QListWidget
+from PySide6.QtCore import Qt
+import requests
+import json
 
+url = "https://api.ivao.aero/v2/tracker/whazzup"
+update_version = "https://api.github.com/repos/alexcaussades/ivao-info/releases"
+
+r = requests.get(url)
+atc = r.json()
+
+x = atc["clients"]["atcs"]
+p = atc["clients"]["pilots"]
 
 class mainWindows(QWidget):
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Sim IVAO Info Serv")
-        self.setWindowOpacity(1)
+        self.resize(400, 400)
+        
+        #Label online ATCs and Pilote
+        self.atc_online = QLabel("ATCs : {0} Online - Pilots : {1} Online".format(len(x), len(p)))
+        self.font = self.atc_online.font()
+        self.font.setPointSize(10)
+        self.atc_online.setFont(self.font)
+        
+        self.version_app = QLabel("Version Alpha 0.0.1")
+        self.version_app.setAlignment(Qt.AlignRight)
+        
+        self.main_w = QGridLayout(self)
+        self.list_ATC = QListWidget()
+        self.af = QLineEdit()
+        self.af.setPlaceholderText("ATCs Online")
+        self.btn_enter = QPushButton("Entrée")
+        self.btn_atc = QPushButton("text")
 
-        main_layout = QHBoxLayout(self)
-        # button = QPushButton("t")
-        # main_layout.addWidget(button)
+        
+        self.main_w.addWidget(self.atc_online, 0, 0, 1, 4)
+        self.main_w.addWidget(self.list_ATC, 1, 0, 1, 3)
+        
+        self.main_w.addWidget(self.af, 2,0,1,1)
+        self.main_w.addWidget(self.btn_enter, 2,2,1,1)
+        self.main_w.addWidget(self.btn_atc, 3, 0, 1, 4)
+        self.main_w.addWidget(self.version_app, 4,2,1,1)
 
-        left_layout = QVBoxLayout()
-        rigth_layout = QVBoxLayout()
-        midlle_l = QHBoxLayout()
-        
-        btn = QPushButton("hello")
-        midlle_l.addWidget(btn)
-        
-        
-        main_layout.addLayout(left_layout)
-        main_layout.addLayout(midlle_l)
-        main_layout.addLayout(rigth_layout)
-        
-        for i in range(1, 11):
-            btn = QPushButton(str(i))
-            left_layout.addWidget(btn)
-        
-        for i in range(1, 11):
-            btn = QPushButton(str(i))
-            rigth_layout.addWidget(btn)
-        
 app = QApplication()
-
 main_windows = mainWindows()
 main_windows.show()
 app.exec()
